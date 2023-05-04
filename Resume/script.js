9504b4cefc07c4b1fd1c3904d6fac0910ea81b07
d <d@ds-MacBook-Pro.local>
2023-05-04 23:03:29 +0200

    2023 update

---
+++ Resume/script.js
@@ -0,0 +1,17 @@
+function showDac1() {
+    var x = document.getElementById("dac1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+  function showLearned() {
+    var x = document.getElementById("things-ive-learnt");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
